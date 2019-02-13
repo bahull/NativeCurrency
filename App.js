957bc0ef664c9a88d5d21ddb59796e23c6e96cc0
Bryce Hull <bahull@live.com>
2019-02-12 19:36:53 -0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Hey Guys!</Text>
+        <Button
+          onPress={() => console.log("Hi")}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
