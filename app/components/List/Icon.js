5113f67bcf1cdcdc1e0c8b1945d8a83ef7fa056c
Bryce Hull <bahull@live.com>
2019-02-15 20:18:04 -0600

    Complete prior to navigation

--- app/components/List/Icon.js
+++ app/components/List/Icon.js
@@ -4,10 +4,13 @@ import { View, Image } from 'react-native';
 
 import styles from './styles';
 
-const Icon = ({ checkmark, visible }) => {
+const Icon = ({ checkmark, visible, iconBackground }) => {
   const iconStyles = [styles.icon];
   if (visible) {
     iconStyles.push(styles.iconVisible);
+  }
+  if (iconBackground) {
+    iconStyles.push({ backgroundColor: iconBackground });
   }
   return (
     <View style={iconStyles}>
@@ -25,6 +28,7 @@ const Icon = ({ checkmark, visible }) => {
 Icon.propTypes = {
   checkmark: PropTypes.bool,
   visible: PropTypes.bool,
+  iconBackground: PropTypes.string,
 };
 
 export default Icon;
--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
-import { ScrollView, StatusBar } from 'react-native';
+import { ScrollView, StatusBar, Platform } from 'react-native';
+
+import { Ionicons } from '@expo/vector-icons';
 
 import { ListItem, Separator } from '../components/List';
+
+const ICON_COLOR = '#868686';
+const ICON_SIZE = 23;
+const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
 
 class Options extends Component {
   handleThemesPress = () => {
@@ -16,6 +22,20 @@ class Options extends Component {
     return (
       <ScrollView>
         <StatusBar translucent={false} barStyle="default" />
+        <ListItem
+          text="Themes"
+          onPress={this.handleThemesPress}
+          customIcon={
+            <Ionicons name={`${ICON_PREFIX}-arrow-forward`} size={ICON_SIZE} color={ICON_COLOR} />
+          }
+        />
+        <Separator />
+        <ListItem
+          text="Fixer.io"
+          onPress={this.handleSitePress}
+          customIcon={<Ionicons name={`${ICON_PREFIX}-link`} color={ICON_COLOR} size={ICON_SIZE} />}
+        />
+        <Separator />
       </ScrollView>
     );
   }
